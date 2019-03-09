7717b23bc97076b8534ea69e90030616f22a1ef8
AnatoliKosarev <anatoli.kosarev@gmail.com>
2019-03-09 16:32:42 +0300

    Linear algorithm added, code refactored for Equation, EquationTests

--- sandbox/src/main/java/ru/stqa/pft/sandbox/Equation.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Equation.java
@@ -14,18 +14,25 @@ public class Equation {
     this.b = b;
     this.c = c;
 
-    double d = b*b - 4*a*c;
+    double d = b * b - 4 * a * c;
 
-    if (d > 0) {
-      n = 2;
-    }
-    else {
-      if (d == 0) {
+    if (a != 0) {                   //если a!=0 анализируем дискриминант
+      if (d > 0) {
+        n = 2;
+      } else if (d == 0) {
         n = 1;
-      }
-      else {
+      } else {
         n = 0;
       }
+
+    } else if (b != 0) {          //если a=0, b!=0, то n=1
+      n = 1;
+
+    } else if (c != 0) {          //если a=0, b=0, с!=0, то n=0
+      n = 0;
+
+    } else {
+      n = -1;                      //если a,b,c = 0 решений бесконечно много, уравнение не имеет смысла
     }
   }
 
