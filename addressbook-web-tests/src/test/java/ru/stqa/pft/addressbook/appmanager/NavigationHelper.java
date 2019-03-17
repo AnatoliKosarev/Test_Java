70e7134f54de2cedaed98db48d69614e5236cd8a
AnatoliKosarev <anatoli.kosarev@gmail.com>
2019-03-17 14:50:43 +0300

    Page transfer optimized for NavigationHelper

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/NavigationHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/NavigationHelper.java
@@ -10,14 +10,30 @@ public class NavigationHelper extends HelperBase {
   }
 
   public void gotoGroupPage() {
+    if (isElementPresent(By.tagName("h1"))
+        && driver.findElement(By.tagName("h1")).getText().equals("Groups")
+        && isElementPresent(By.name("new"))) {
+      return;
+    } else {
     click(By.linkText("groups"));
+    }
   }
 
   public void gotoContactPage() {
+    if (isElementPresent(By.tagName("h1"))
+        && driver.findElement(By.tagName("h1")).getText().equals("Edit / add address book entry")
+        && isElementPresent(By.name("new_group"))) {
+      return;
+    } else {
     click(By.linkText("add new"));
+    }
   }
 
   public void gotoHomePage() {
+    if (isElementPresent(By.id("maintable"))) {
+    return;
+    } else {
     click(By.linkText("home"));
+    }
   }
 }
