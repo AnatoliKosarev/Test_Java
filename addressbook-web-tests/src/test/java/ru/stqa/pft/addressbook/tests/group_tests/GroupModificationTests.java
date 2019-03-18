c6fd17e4c52b43de4845026509d71a51748a7b1b
AnatoliKosarev <anatoli.kosarev@gmail.com>
2019-03-18 23:05:55 +0300

    IsThereAGroup check implemented for GroupDeletion, GroupModification tests

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/group_tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/group_tests/GroupModificationTests.java
@@ -9,6 +9,11 @@ public class GroupModificationTests extends TestBase {
   @Test
   public void testGroupModification() {
     app.getNavigationHelper().gotoGroupPage();
+
+    if (! app.getGroupHelper().isThereAGroup()) {
+      app.getGroupHelper().createGroup(new GroupData("test1", null, null));
+    }
+
     app.getGroupHelper().selectGroup();
     app.getGroupHelper().initGroupModification();
     app.getGroupHelper().fillGroupForms(new GroupData("test1", "test2", "test3"));
